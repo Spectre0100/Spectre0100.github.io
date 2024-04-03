82dc3648b7a9b2997659fdf049f6a13ce963c828
Eric Wang <89584602+Spectre0100@users.noreply.github.com>
2024-04-03 01:11:16 +0100

    UI overhaul, new loading and status messages
    Cleaner streamlined interface.
    No longer refreshing page for API calls. 
    Added loading animations while fetching and status messages for when no data is received e.g. service is closed.
    Now displays seconds if less than 60 seconds to arrival.
    Switched from JSFiddle to Replit.

---
+++ TFLTimes/script.js
@@ -0,0 +1,101 @@
+// URLs for TFL API calls
+const url_bla = 'https://api.digital.tfl.gov.uk/Line/dlr/Arrivals/940GZZDLBLA?direction=inbound';
+const url_twg = 'https://api.digital.tfl.gov.uk/stoppoint/940GZZDLTWG/Arrivals';
+const url_can = 'https://api.tfl.gov.uk/Line/elizabeth/Arrivals/910GCANWHRF?direction=outbound';
+const url_cyf = 'https://api.tfl.gov.uk/Line/jubilee/Arrivals/940GZZLUCYF?direction=inbound';
+const url_gpk = 'https://api.tfl.gov.uk/Line/jubilee/Arrivals/940GZZLUGPK?direction=outbound';
+
+// Simplifies station names
+getName = function(s) {
+  return s.replace(/ Underground| Station| Rail| DLR/g, '')
+}
+
+// Set timer between fetch requests
+function timeout(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// Get and write the arrivals data
+async function getArrivalsTable(stationUrl, stationCode) {
+  try {
+    // Show loading while fetching data
+    loading = document.getElementById(`${stationCode}loading`);
+    loading.style.display = "block";
+
+    // Fetch data
+    const response = await fetch(stationUrl);
+    const data = await response.json();
+
+    // Hide loading message when data is fetched
+    loading.style.display = "none";
+
+    // Retrieved no data, e.g. service is closed 
+    if (data.length == 0) {
+      document.getElementById(`${stationCode}Status`).innerHTML = "No data.";
+      return;
+    }
+
+    // Format arrivals data
+    let arrivals = [];
+    for (const arrival of data) {
+      // If less than 60 seconds display seconds, else minutes 
+      seconds = arrival.timeToStation;
+      // Flag to store whether seconds should be displayed
+      var secFlag = function() { return seconds < 60 };
+      if (secFlag) {
+        arrivals.push({
+          'dest': getName(arrival.destinationName),
+          'time': seconds
+        });
+      } else {
+        arrivals.push({
+          'dest': getName(arrival.destinationName),
+          'time': Math.round(arrival.timeToStation / 60)
+        });
+      }
+    }
+    // Sort arrivals closest first and display next 3 max
+    arrivals.sort((a, b) => a.time - b.time);
+    arrivals = arrivals.slice(0, 3);
+    // console.log(arrivals);
+
+    // Clear table to update with new data
+    const tableBody = document.getElementById(stationCode);
+    tableBody.innerHTML = "";
+
+    // Write to table
+    for (const arrival of arrivals) {
+      const row = document.createElement('tr');
+      const destinationCell = document.createElement('td');
+      destinationCell.textContent = arrival.dest;
+      const timeToArrivalCell = document.createElement('td');
+      timeToArrivalCell.textContent = secFlag ? arrival.time + " sec" : arrival.time + " min";
+      row.appendChild(destinationCell);
+      row.appendChild(timeToArrivalCell);
+      tableBody.appendChild(row);
+    }
+
+  } catch (error) {
+    // Error fetching data
+    console.log(error);
+
+    // DLR stations may encounter TypeErrors when fetching
+    if (error instanceof TypeError) {
+      document.getElementById(`${stationCode}Status`).innerHTML = "Failed to fetch.";
+      return;
+    }
+    
+    throw new Error("Error fetching arrivals data.");
+  }
+
+  // Fetch new data every 5 seconds
+  await timeout(5000);
+  getArrivalsTable(stationUrl, stationCode);
+}
+
+// Call function to populate arrivals tables
+getArrivalsTable(url_bla, 'BLA');
+getArrivalsTable(url_twg, 'TWG');
+getArrivalsTable(url_can, 'CAN');
+getArrivalsTable(url_cyf, 'CYF');
+getArrivalsTable(url_gpk, 'GPK');
